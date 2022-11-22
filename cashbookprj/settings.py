6c2ec394b6a76635659bf35d0ba507809ca704fd
yeeun0702 <kimyeeun0702@naver.com>
2022-11-22 20:34:53 +0900

    settings.py 수정

--- cashbookprj/settings.py
+++ cashbookprj/settings.py
@@ -22,17 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-secret_file = os.path.join(BASE_DIR, 'secrets.json')
+# secret_file = os.path.join(BASE_DIR, 'secrets.json')
 
-with open(secret_file) as f:
-    secrets = json.loads(f.read())
+# with open(secret_file) as f:
+#     secrets = json.loads(f.read())
 
-def get_secret(setting, secrets=secrets):
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = "Set the {} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
+# def get_secret(setting, secrets=secrets):
+#     try:
+#         return secrets[setting]
+#     except KeyError:
+#         error_msg = "Set the {} environment variable".format(setting)
+#         raise ImproperlyConfigured(error_msg)
 
 SECRET_KEY = 'django-insecure-=-y4d3ns2j6fq1g3p@mmg0!8k5izu20#+idlo^syr)3b9wgbd$'
 # SECURITY WARNING: don't run with debug turned on in production!
